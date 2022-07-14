66b6038f9d2381d2cd911ad694fb1c00f829d341
CSarroca <sarroca93@gmail.com>
2022-07-14 20:19:16 +0200

    added firts class array creator

---
+++ js/Array/Skylabarray.js
@@ -0,0 +1,15 @@
+class Skylabarray {
+  index0;
+  index1;
+  index2;
+  index3;
+
+  constructor(index0, index1, index2, index3) {
+    this.index0 = index0;
+    this.index1 = index1;
+    this.index2 = index2;
+    this.index3 = index3;
+  }
+}
+
+module.exports = Skylabarray;
