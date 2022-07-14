9d1c044478300b733e73184ffe71ea129056c279
CSarroca <sarroca93@gmail.com>
2022-07-14 20:39:41 +0200

    added 1 more index

--- js/Array/Skylabarray.js
+++ js/Array/Skylabarray.js
@@ -3,12 +3,14 @@ class Skylabarray {
   index1;
   index2;
   index3;
+  index4;
 
-  constructor(index0, index1, index2, index3) {
+  constructor(index0, index1, index2, index3, index4) {
     this.index0 = index0;
     this.index1 = index1;
     this.index2 = index2;
     this.index3 = index3;
+    this.index4 = index4;
   }
 }
 
